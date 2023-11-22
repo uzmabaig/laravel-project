df8f29c9ab09ef55e26500a7b28eb6806494b062
uzmabaig <uzmaa.ba@outlook.com>
2023-11-22 22:05:28 +0500

    first and completed project

---
+++ resources/views/myfile.blade.php
@@ -0,0 +1,1 @@
+<h3> You are my whole world!</h3>
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/myfile',function(){
-//     return view('contentfile');
-    
-// });
+
 
 
 Route::get('/first-project',function(){
     return view('new-project');
 });
 
-Route::view('/myfile','contentfile');
+Route::view('contentfile','myfile');
+
 
 
